75be4144ed36e978f09abb35c90843c5206eca2e
jonathan930112 <mine19765craftj74561@gmail.com>
2020-06-16 19:02:14 +0800

    1

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print("hello world!")
+import tensorflow as tf
+
+import tensorflow.examples.tutorials.mnist.input_data as input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+print('train',mnist.train.num_examples,
+      ',validation',mnist.validation.num_examples,
+      ',test',mnist.test.num_examples)
